4f78d768b7ac7dbc840ab7c4c5c56803dd483832
Megan McNeill <mcneim2@gmail.com>
2024-06-11 10:19:06 -0400

    test pull 1 piece of data successful

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
-async function apiTest() {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9025870b58f55c244123e7bc18ed93ea");
-    const movies = await response.json();
-    console.log(movies);
-    }
+// async function apiTest() {
+//     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9025870b58f55c244123e7bc18ed93ea");
+//     const movies = await response.json();
+//     console.log(movies);
+//     }
  
-apiTest()
+// apiTest()
+
+const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9025870b58f55c244123e7bc18ed93ea';
+
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('test test test \n----------');
+    console.log(data.name)
+    // for (let i = 0; i < data.length; i++) {
+    //   console.log(data[i].name);
+    // }
+  });
