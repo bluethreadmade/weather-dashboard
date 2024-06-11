8515bd9bb05528e38ff98d9729336a72c0213af9
Megan McNeill <mcneim2@gmail.com>
2024-06-11 15:23:09 -0400

    search button event listener, clearing search bar, assign a variable to search bar input

--- assets/script.js
+++ assets/script.js
@@ -6,16 +6,34 @@
  
 // apiTest()
 
-const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9025870b58f55c244123e7bc18ed93ea';
+// const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9025870b58f55c244123e7bc18ed93ea';
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('test test test \n----------');
-    console.log(data.name)
-    // for (let i = 0; i < data.length; i++) {
-    //   console.log(data[i].name);
-    // }
-  });
+// fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log('test test test \n----------');
+//     console.log(data.name)
+//     // for (let i = 0; i < data.length; i++) {
+//     //   console.log(data[i].name);
+//     // }
+//   });
+
+
+// takes the text entered in the search bar and assigns it to a variable when the search button is clicked
+  function searchSubmit(event) {
+    event.preventDefault();
+
+    const searchInput = $('#search-input').val();
+    
+    console.log("searched");
+    console.log(searchInput);
+
+    // clears the search bar
+    $('#search-input').val('');
+
+};
+  
+// Search button event listener
+$('#search-bar').on('submit', searchSubmit);
