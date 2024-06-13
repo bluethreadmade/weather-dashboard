ecfac049fd4804386278bd7114c1746479b9eabe
Megan McNeill <mcneim2@gmail.com>
2024-06-12 21:24:32 -0400

    added for loop for the five day forecase, re-arranged dayCard elements, added icon via url

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,7 @@
 //   });
 let currentWeather = {};
 let oneDay = {};
+let fiveDay = [];
 
 // takes the text entered in the search bar and assigns it to a variable when the search button is clicked
 function searchSubmit(event) {
@@ -45,83 +46,94 @@ function searchSubmit(event) {
       }
     })
      .then(function (data) {
-          // console.log(data.name);
-          // console.log(data.coord.lat);
-          // console.log(data.coord.lon);
-          // console.log(data.main.temp);
-          // console.log(data.main.humidity);
-          // console.log(data.weather[0].icon);
-          // console.log(data.wind.speed);
-          // console.log(data.dt);
+        // console.log(data.name);
+        // console.log(data.coord.lat);
+        // console.log(data.coord.lon);
+        // console.log(data.main.temp);
+        // console.log(data.main.humidity);
+        // console.log(data.weather[0].icon);
+        // console.log(data.wind.speed);
+        // console.log(data.dt);
 
-          const searchLat = data.coord.lat; 
-          const searchLon = data.coord.lon;
+        const searchLat = data.coord.lat; 
+        const searchLon = data.coord.lon;
 
-          const currentCity = data.name;
-          const currentTemp = data.main.temp;
-          const currentHumidity = data.main.humidity;
-          const currentIcon = data.weather[0].icon;
-          const currentWind = data.wind.speed;
-          const currentTime = data.dt;
+        const currentCity = data.name;
+        const currentTemp = data.main.temp;
+        const currentHumidity = data.main.humidity;
+        const currentIcon = data.weather[0].icon;
+        const currentWind = data.wind.speed;
+        const currentTime = data.dt;
 
 
-          // create array of current data
-          const currentWeather = {
-            city: currentCity,
-            temp: currentTemp,
-            humidity: currentHumidity,
-            icon: currentIcon,
-            wind: currentWind,
-            time: currentTime,
-          };
+        // create array of current data
+        const currentWeather = {
+          city: currentCity,
+          temp: currentTemp,
+          humidity: currentHumidity,
+          icon: currentIcon,
+          wind: currentWind,
+          time: currentTime,
+        };
 
-          console.log(currentWeather);
+        console.log(currentWeather);
 
         const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${searchLat}&lon=${searchLon}&APPID=9025870b58f55c244123e7bc18ed93ea&units=imperial`
       
         fetch(weatherApiUrl)
-      .then(function (weatherResponse) {
-        if (weatherResponse.ok) {
-          return weatherResponse.json();
-        } else {
-          throw new Error('Failed to fetch weather data')
-        }
-      })
-      .then(function (weatherData) {
-          console.log(weatherData);
-
-            for(i=0; i< weatherData.list.length; i+=8) {
-              console.log(weatherData.list[i].dt_txt);
-
-              const dayTemp = weatherData.list[i].main.temp;
-              const dayHumidity = weatherData.list[i].main.humidity;
-              const dayIcon = weatherData.list[i].weather[0].icon;
-              const dayWind = weatherData.list[i].wind.speed;
-              const dayDate = weatherData.list[i].dt_txt;
-              
-              const oneDay = {
-                temp: dayTemp,
-                humidity: dayHumidity,
-                icon: dayIcon,
-                wind: dayWind,
-                date: dayDate
-              };
-              
-              return oneDay;
+          .then(function (weatherResponse) {
+            if (weatherResponse.ok) {
+              return weatherResponse.json();
+            } else {
+              throw new Error('Failed to fetch weather data')
             }
-            
-        });
+          })
+          .then(function (weatherData) {
+              console.log("forecast",weatherData.list);
+
+              // declare an array
+
+                for(let i=0; i< weatherData.list.length; i+=8) {
+                  //console.log(weatherData.list[i].dt_txt);
+
+
+                  const dayTemp = weatherData.list[i].main.temp;
+                  const dayHumidity = weatherData.list[i].main.humidity;
+                  const dayIcon = weatherData.list[i].weather[0].icon;
+                  const dayWind = weatherData.list[i].wind.speed;
+                  const dayDate = weatherData.list[i].dt_txt;
+                  
+                  const oneDay = {
+                    temp: dayTemp,
+                    humidity: dayHumidity,
+                    icon: dayIcon,
+                    wind: dayWind,
+                    date: dayDate
+                  };
+                  
+                  console.log("one day", oneDay);
+
+                  // push new object to array
+                  fiveDay.push(oneDay);
+                  
+                  
+                  }
+                  
+                  
+                  createCurrentWeatherArticle(currentWeather);
+                  // pass array to create day cards
+                  createDayCards(fiveDay);
+                  console.log("five day", fiveDay);
+                
+          });
 
       })
       .catch(function (error) {
           console.error('Error featching geographical coordinated:', error);
       });
     
-     createCurrentWeatherArticle(currentWeather);
-     createDayCards(oneDay);
   };
 
-    
 
 function createCurrentWeatherArticle(currentWeather) {
 
@@ -155,49 +167,52 @@ function createCurrentWeatherArticle(currentWeather) {
   return currentWeatherArticle;
 };
 
-function createDayCards() {
+function createDayCards(fiveDay) {
 
   console.log("5 day")
+// loop through 5
+for (let i = 0; i < fiveDay.length; i++) {
+  const element = fiveDay[i];
 
-  const dayCard = $('<div>')
+    const dayCard = $('<div>')
     .addClass('card')
     //.addStyle('width: 18rem;')   
     .appendTo("#five-day");
 
-  const dayCardList = $('<ul>')
-    .addClass('list-group list-group-flush')
-    .appendTo("#five-day");
+    const dayCardDate = $('<div>')
+      .addClass('card-header')
+      .text(element.date)
+      .appendTo("#five-day");
+      
+    const dayCardIcon = $('<li>')
+      .addClass('list-group-item')
+      .append($('<img>').attr('src', `https://openweathermap.org/img/wn/${element.icon}@2x.png`))
+      .appendTo("#five-day");
 
-  const dayCardTemp = $('<li>')
-    .addClass('list-group-item')
-    .text(oneDay.dayTemp)
-    .appendTo("#five-day");
+    const dayCardList = $('<ul>')
+      .addClass('list-group list-group-flush')
+      .appendTo("#five-day");
 
-  const dayCardHumidity = $('<li>')
-    .addClass('list-group-item')
-    .text(oneDay.dayHumidity)
-    .appendTo("#five-day");
+    const dayCardTemp = $('<li>')
+      .addClass('list-group-item')
+      .text('Temperature: ' + element.temp)
+      .appendTo("#five-day");
 
-  const dayCardIcon = $('<li>')
-    .addClass('list-group-item')
-    .text(oneDay.dayIcon)
-    .appendTo("#five-day");
+    const dayCardHumidity = $('<li>')
+      .addClass('list-group-item')
+      .text('Humidity: ' + element.humidity)
+      .appendTo("#five-day");
 
-  const dayCardWind = $('<li>')
-    .addClass('list-group-item')
-    .text(oneDay.dayWind)
-    .appendTo("#five-day");
+    const dayCardWind = $('<li>')
+      .addClass('list-group-item')
+      .text('Wind Speed: ' + element.wind)
+      .appendTo("#five-day");
 
-  const dayCardDate = $('<li>')
-    .addClass('list-group-item')
-    .text(oneDay.dayDate)
-    .appendTo("#five-day");
 
-  return dayCard;
 };
 
+};
 
-
-  
 // Search button event listener
 $('#search-bar').on('submit', searchSubmit)
+
